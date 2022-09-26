d5d03c9bc6143de662459025f4343663731cf5b1
User <j_camacho8@yahoo.com>
2022-09-25 22:01:06 -0700

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,13 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/', (req,res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/burger.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'images/burger.jpg'
-      }]
-      
-      res.render('places/index', { places })
-})
+// GET /places
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
@@ -37,6 +21,44 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id})
+  }
+})
+
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+
 
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,20 +1,16 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
-
 module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
+    pic: '/images/thai.jpg'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
+    pic: '/images/cafe.jpg'
 }]
