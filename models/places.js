4488b6dbb1b61e759c375f17c0c9cd602bf70c35
User <j_camacho8@yahoo.com>
2022-09-30 20:13:10 -0700

    intergrated mongoose

--- models/places.js
+++ models/places.js
@@ -1,16 +1,12 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const mongoose = require('mongoose')
 
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cafe.jpg'
-}]
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
